c27e433980365fdc7b16f90b0529ca0f7d0c69af
Nartyna <mplutwska@gmail.com>
2022-08-25 20:13:15 +0200

    clean up in folders and day cycle work (time, weather, seasons)

---
+++ Enemies/Base/BaseEnemy.gd
@@ -0,0 +1,95 @@
+class_name Enemy 
+extends KinematicBody2D
+
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var WANDER_TARGET_RAGE = 10
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+
+var state = IDLE
+
+onready var sprite = $AnimatedSprite
+onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
+onready var softCollision = $SoftCollision
+onready var wanderController = $WanderControler
+onready var animationPlayer = $BlinkEffect
+
+
+func _ready():
+	state = pick_random_state([IDLE, WANDER])
+
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				random_state()
+		WANDER:
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				random_state()
+			accelerate_towards_point(wanderController.target_position, delta)
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RAGE:
+				random_state()
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				accelerate_towards_point(player.global_position, delta)
+				
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 500
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+
+func random_state():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1,3))
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	knockback = area.knockback_vector * 130
+	hurtbox.create_hit_effect()
+	hurtbox.start_invincibility(0.4)
+
+func _on_Stats_no_health():
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
+
+func _on_Hurtbox_invincibility_started():
+	animationPlayer.play("Start")
+
+func _on_Hurtbox_invincibility_ended():
+	animationPlayer.play("Stop")
--- MinimapCamera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-#onready var player = $"../ViewportContainer/Viewport/World/Camera2D"
-onready var player = get_node("../../../ViewportContainer/Viewport/World/YSort/Player")
-func _ready():
-	transform.origin.x = player.transform.origin.x
-	transform.origin.y = player.transform.origin.y

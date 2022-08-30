5d35fdb25e6187b943552260f5f9ced85467664c
Nartyna <mplutwska@gmail.com>
2022-08-30 22:34:45 +0200

    finally nicer clouds and move thunder from dayNight to weather, semi working

--- DayCycle/DayNight.gd
+++ DayCycle/DayNight.gd
@@ -1,7 +1,6 @@
 extends CanvasModulate
 
 const seconds_per_day:int = 86400 # 60 * 60 * 24
-const seconds_per_hour:int = 3600 # 60 * 60
 const HOUR = 60 * 60
 export(float, 0, 1) var brightness_night = 0.3
 export(float, 0, 1) var brightness_day = 1.0
@@ -10,8 +9,8 @@ export(Color) var color_day = Color("fff1d0")
 export(Color) var color_dusk = Color("854646")
 export(Color) var color_night = Color("27264c")
 
-var dawn_start:float = HOUR* 4.0 # starts at 4 o'clock
-var dusk_start:float = HOUR* 18.0 # start at 18 o'clock
+var dawn_start:float = HOUR * 4.0 # starts at 4 o'clock
+var dusk_start:float = HOUR * 18.0 # start at 18 o'clock
 
 var sunrise_start:float
 var sunrise_end:float
@@ -20,16 +19,7 @@ var sunset_end:float
 
 var color_gradient:Gradient = Gradient.new()
 
-var color_add:Color = Color(0, 0, 0, 0)
-
 var current_color:Color = color_night
-
-var cloudiness_factor:float = 0
-var thunderstorm_enabled:bool = false
-var thunderstorm_strength:float = 0.2
-var lightning_strike_on:bool = false
-
-var thunder_random:RandomNumberGenerator = RandomNumberGenerator.new()
 
 enum TimeOfDay{
 	AUTO,
@@ -57,45 +47,28 @@ func _ready():
 	var night_start = float(dusk_start + (HOUR * 3)) / seconds_per_day
 	
 	color_gradient.remove_point(1)
-	color_gradient.set_color(0.0, color_night)
+	color_gradient.set_color(0, color_night)
 	color_gradient.add_point(sunrise_start, color_night)
 	color_gradient.add_point(sunrise_end, color_dawn)
 	color_gradient.add_point(day_start, color_day)
 	color_gradient.add_point(sunset_start, color_day)
 	color_gradient.add_point(sunset_end, color_dusk)
 	color_gradient.add_point(night_start, color_night)
-#	$Sprite.texture.gradient = color_gradient
-#	print(color_gradient.interpolate(sunrise_start+0.01).to_html(false))
 	
 # when time passes, we calculate the brightness and tint of the environment
 # when time passes, we calculate the brightness and tint of the environment
 # based on the current time of day
-func _on_time_passed(t):
+func _on_time_passed(_t):
 	if time_of_day != TimeOfDay.AUTO:
 		return
 	var time = Clock.time
 
-	var time_of_day = int(time) % seconds_per_day
-	current_color = color_gradient.interpolate(float(time_of_day) / seconds_per_day)
-
+	var day_time = int(time) % seconds_per_day
+	current_color = color_gradient.interpolate(float(day_time) / seconds_per_day)
 	# apply color
 	color = current_color
 
-	# add color add
-	color += color_add
-	# add cloudiness
-	if cloudiness_factor > 0.5:
-		var a = (cloudiness_factor - 0.5) * 2.0
-		color = color.linear_interpolate(color.darkened(0.5), a)
-
-
-func lightning_strike():
-	lightning_strike_on = true
-	var a = rand_range(0.2, 0.8)
-	color_add = Color(a, a, a, 0)
-
-
-func _process(delta):
+func _process(_delta):
 	if time_of_day != TimeOfDay.AUTO:
 		match time_of_day:
 			"DAWN":
@@ -106,13 +79,3 @@ func _process(delta):
 				color = color_dusk
 			"NIGHT":
 				color = color_night
-	if thunderstorm_enabled:
-		if lightning_strike_on:
-			if color_add.r <= 0.01:
-				lightning_strike_on = false
-			else:
-				color_add *= 0.9
-		else:
-			var rand = abs(thunder_random.randfn(0, 1))
-			if rand < thunderstorm_strength * 0.1:
-				lightning_strike()
--- DayCycle/Weather.gd
+++ DayCycle/Weather.gd
@@ -1,23 +1,48 @@
 extends Node
 
-onready var dayNight = get_node("../DayNight")
-onready var cloudLayer = $CloudLayer
+onready var thunderLayer = $ThunderLayer
+onready var cloudsLayer = $CloudsLayer
 onready var rainParticles = $RainParticles
 
-export(bool) var rain = false setget start_rain
+export(bool) var rain = false setget set_rain
 export(int) var rain_amount = 2000 setget set_rain_amount
-export(bool) var thunder = false setget start_thunderstorm
+
+export(float, 0, 1) var cloud_speed:float = 0.03 setget set_cloud_speed
+export(float, 0, 1) var cloudiness:float = 2 setget set_cloudiness
+
+export(bool) var thunder = false
 export(float, 0, 1) var thunder_strength_rand_min:float = 0.2
 export(float, 0, 1) var thunder_strength_rand_max:float = 1
-export(float, 0, 1) var cloud_speed:float = 0.1 setget set_cloud_speed
-export(float, 0, 1) var cloudiness_factor:float = 0.5 setget set_cloudiness
+var lightning_strike_on:bool = false
+var color_add:Color = Color(0, 0, 0, 0)
 export(int) var time_multipler = 3600 setget set_time_multipler
-var thunder_strength:float
 
-func _process(delta):
-	thunder_strength = rand_range(thunder_strength_rand_min, thunder_strength_rand_max)
+func _process(_delta):
+	if thunder:
+		var thunder_strength = rand_range(thunder_strength_rand_min, thunder_strength_rand_max)
+		if lightning_strike_on:
+			if color_add.r <= 0.01:
+				lightning_strike_on = false
+			else:
+				color_add *= 0.9
+		else:
+			print("A")
+			lightning_strike()
+#			var thunder_random = RandomNumberGenerator.new()
+#			var rand = abs(thunder_random.randfn(0, 1))
+#			if rand < thunder_strength * 0.1:
+#				lightning_strike()
+		thunderLayer.color += color_add
+		var timer = get_tree().create_timer(2)
+		if timer.time_left >= 0:
+			thunderLayer.color = Color("ffffff")
 
-func start_rain(new_value):
+func lightning_strike():
+	lightning_strike_on = true
+	var a = rand_range(0.2, 0.8)
+	color_add = Color(a, a, a, 0)
+
+func set_rain(new_value):
 	rain = new_value
 	if rainParticles:
 		rainParticles.amount = rain_amount
@@ -27,25 +52,13 @@ func set_rain_amount(new_value):
 	rain_amount = new_value
 	rainParticles.amount = rain_amount
 
-func start_thunderstorm(new_value):
-	thunder = new_value
-	if dayNight:
-		dayNight.thunderstorm_strength = thunder_strength
-		dayNight.thunderstorm_enabled = new_value
-
 func set_cloud_speed(new_value):
 	cloud_speed = new_value
-	cloudLayer.get_node("ParallaxLayer/AnimationPlayer").playback_speed = cloud_speed
+	cloudsLayer.shader_param.time_scale_1 = new_value
 
 func set_cloudiness(new_value):
-	cloudiness_factor = new_value
-	var clouds = cloudLayer.get_node("ParallaxLayer")
-	if cloudiness_factor < 0.5:
-		clouds.modulate.a = cloudiness_factor
-	else:
-		clouds.modulate.a = 1 - cloudiness_factor
-	
-	dayNight.cloudiness_factor = cloudiness_factor
+	cloudiness = new_value
+	cloudsLayer.shader_param.tile_factor_1 = cloudiness
 	
 func set_time_multipler(new_value):
 	time_multipler = new_value

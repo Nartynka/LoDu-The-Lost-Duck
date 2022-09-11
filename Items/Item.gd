43220226f5420f2e0fb4c3daa7ef6248cb5c9d80
Nartyna <mplutwska@gmail.com>
2022-09-11 22:16:35 +0200

    fix bugs with items and resized item sprites

--- Items/Item.gd
+++ Items/Item.gd
@@ -7,5 +7,7 @@ func _on_Item_body_entered(body):
 	if body.name == "Player":
 		$AudioStreamPlayer.play()
 		Inventory.add_item(item_name, 1)
-		yield(get_tree().create_timer(0.4), "timeout")
+		hide()
+		set_deferred("monitoring", false)
+		yield(get_tree().create_timer(1), "timeout")
 		queue_free()

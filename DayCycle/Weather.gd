16f2ffcce35f5e9e35dacc89181f99d8d5b24d62
Nartyna <mplutwska@gmail.com>
2022-10-30 21:24:42 +0100

    Fix rain

--- DayCycle/Weather.gd
+++ DayCycle/Weather.gd
@@ -1,14 +1,14 @@
 extends Node
 
 onready var thunderLayer = $ThunderLayer
-onready var cloudsLayer = $CloudsLayer
+#onready var cloudsLayer = $CloudsLayer
 onready var rainParticles = $RainParticles
 
 export(bool) var rain = false setget set_rain
-export(int) var rain_amount = 2000 setget set_rain_amount
+export(int) var rain_amount = 20000 setget set_rain_amount
 
-export(float, 0, 1) var cloud_speed:float = 0.03 setget set_cloud_speed
-export(float, 0, 1) var cloudiness:float = 2 setget set_cloudiness
+#export(float, 0, 1) var cloud_speed:float = 0.03 setget set_cloud_speed
+#export(float, 0, 1) var cloudiness:float = 2 setget set_cloudiness
 
 export(bool) var thunder = false
 export(float, 0, 1) var thunder_strength_rand_min:float = 0.2
@@ -40,24 +40,27 @@ func lightning_strike():
 	var a = rand_range(0.2, 0.8)
 	color_add = Color(a, a, a, 0)
 
+func set_rain_amount(new_value):
+	rain_amount = new_value
+	if rainParticles:
+		rainParticles.amount = rain_amount
+
 func set_rain(new_value):
 	rain = new_value
 	if rainParticles:
 		rainParticles.amount = rain_amount
-		rainParticles.set_emitting(new_value)
+		rainParticles.set_emitting(rain)
 
-func set_rain_amount(new_value):
-	rain_amount = new_value
-	rainParticles.amount = rain_amount
-
-func set_cloud_speed(new_value):
-	cloud_speed = new_value
-	cloudsLayer.shader_param.time_scale_1 = new_value
-
-func set_cloudiness(new_value):
-	cloudiness = new_value
-#	cloudsLayer.shader_param.tile_factor_1 = cloudiness
-	
 func set_time_multipler(new_value):
 	time_multipler = new_value
 	Clock.time_multiplier = new_value
+
+
+#func set_cloud_speed(new_value):
+#	cloud_speed = new_value
+#	cloudsLayer.shader_param.time_scale_1 = new_value
+
+#func set_cloudiness(new_value):
+#	cloudiness = new_value
+#	cloudsLayer.shader_param.tile_factor_1 = cloudiness
+	

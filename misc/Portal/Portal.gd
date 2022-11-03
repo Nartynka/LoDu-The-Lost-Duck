542b388fca6d209f0d72b95d98f2c1292e33b503
Nartyna <mplutwska@gmail.com>
2022-11-03 21:59:43 +0100

    change export variable name and add portal to world in new world

--- misc/Portal/Portal.gd
+++ misc/Portal/Portal.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 export(String, FILE, "*.tscn") var to_scene = ""
-export(String) var spawnpoint = "spawnpoint"
+export(String) var to_spawnpoint = "spawnpoint"
 export(String) var spawnpoint_name = "spawnpoint"
 
 # Portal MUST be placed above player in scene tree, idk why but doesn't work if below player
 # Portal is in Portals gorup, spawnpoint is in spawnpoints group
 
 func _ready():
-	if spawnpoint == "spawnpoint":
-		spawnpoint = get_tree().current_scene.name+"Pos"
+	if to_spawnpoint == "spawnpoint":
+		to_spawnpoint = get_tree().current_scene.name+"Pos"
 	if spawnpoint_name == "spawnpoint":
 		spawnpoint_name = name+"Pos"
 	if is_in_group("portals"):
@@ -20,6 +20,6 @@ func _on_Portal_body_entered(body):
 		if  to_scene == "":
 			push_error("Error changing scenes: to_scene has no assigned scene")
 			return false
-		PlayerStats.spawnpoint = spawnpoint
+		PlayerStats.spawnpoint = to_spawnpoint
 		PlayerStats.previous_scene = get_tree().current_scene.name
 		SceneChange.change_scene(to_scene)
